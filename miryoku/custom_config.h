e79e0d697aa69c5867c0da84bc0fc9bcfd74bcc7
Linus Hochbaum <linus.hochbaum@bridgefield.de>
2024-08-16 13:03:53 +0200

    Bind SQT and DQT

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,3 +3,10 @@
 
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_CLIPBOARD_WIN
+
+// Bind single and double quotes to symbol layer
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &kp DQT,           &kp SQT,           &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
